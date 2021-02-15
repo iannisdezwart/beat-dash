cb5d442a7c122f6ea7d95d39564f56fa3913cd76
iannisdezwart <iannis__@hotmail.com>
2021-02-15 20:52:45 +0100

    🚀 Created Level Class

---
+++ public/js/level.ts
@@ -0,0 +1,30 @@
+abstract class Level {
+	songFileName: string
+	song = new Sound()
+	game: Game
+	bpm: number
+
+	constructor(songFileName: string, bpm: number) {
+		this.songFileName = songFileName
+		this.bpm = bpm
+		this.game = new Game('#game', this)
+		this.load()
+	}
+
+	abstract loadLevel(): void
+	abstract onLoaded(): void
+
+	async load() {
+		this.loadLevel()
+		await this.loadSong()
+		this.onLoaded()
+	}
+
+	async loadSong() {
+		const songURL = await Sound.getFile(this.songFileName, e => {
+			console.log((e.loaded / e.total * 100).toFixed(1) + '%')
+		})
+
+		this.song.load(songURL)
+	}
+}
