bfb3fd0b7d9187be8ca85d0691087a6ce08f6535
iannisdezwart <iannis__@hotmail.com>
2021-02-15 20:54:47 +0100

    🛠 Mostly Bug Fixes

--- public/js/sprites/platform.ts
+++ public/js/sprites/platform.ts
@@ -2,33 +2,78 @@ class Platform extends Sprite {
 	x: number
 	width: number
 	height: number
+	onCeiling: boolean
+
 	topLeft: Vector
 	bottomRight: Vector
+
 	playerPassed = false
 
-	constructor(x: number, width: number, height: number) {
+	constructor(x: number, width: number, height: number, onCeiling = false) {
 		super()
-		this.x = x
+		this.x = x + Player.leftDelay
 		this.width = width
 		this.height = height
+		this.onCeiling = onCeiling
 	}
 
 	render(game: Game) {
-		this.topLeft = new Vector([ this.x, Floor.y - this.height ])
-		this.bottomRight = new Vector([ this.x + this.width, Floor.y ])
+		// Render floor platform
 
-		game.fillRect(this.topLeft, this.bottomRight, '#7777ff')
+		if (!this.onCeiling) {
+			this.topLeft = new Vector([ this.x, Floor.y - this.height ])
+			this.bottomRight = new Vector([ this.x + this.width, Floor.y ])
+		}
+
+		// Render ceiling platform
+
+		else {
+			this.topLeft = new Vector([ this.x, Ceiling.y ])
+			this.bottomRight = new Vector([ this.x + this.width, Ceiling.y + this.height ])
+		}
+
+		game.fillRect(this.topLeft, this.bottomRight, '#3a86ff')
 	}
 
 	collision(player: Player) {
+		// If the player is within the x positions of the platform
+
 		if (
-			player.pos.x + Player.radius > this.topLeft.x
-			&& player.pos.x - Player.radius < this.bottomRight.x
+			player.right() > this.topLeft.x
+			&& player.left() < this.bottomRight.x
 		) {
-			if (!this.playerPassed && player.pos.y + Player.radius > this.topLeft.y) {
-				alert('you died')
-			} else if (player.pos.y + Player.radius > this.topLeft.y) {
-				player.pos.y = this.topLeft.y - Player.radius
+			// If gravity is normal
+
+			if (player.gravityMultiplier >= 0) {
+				// If the player collides with the left of the platform, kill them
+
+				if (!this.playerPassed && player.bottom() > this.topLeft.y) {
+					player.kill()
+				}
+
+				// If this is a floor platform, put the player on top of the platform
+
+				if (player.bottom() > this.topLeft.y && !this.onCeiling) {
+					player.pos.y = this.topLeft.y - Player.radius
+					player.vel = 0
+				}
+			}
+
+			// If gravity is inverted
+
+			else {
+				// If the player collides with the left of the platform, kill them
+
+				if (!this.playerPassed && player.top() < this.bottomRight.y) {
+					player.kill()
+				}
+
+				// If this is a ceiling platform, put the player on the bottom of the platform
+
+				if (player.top() < this.bottomRight.y && this.onCeiling) {
+					player.pos.y = this.bottomRight.y + Player.radius
+					player.vel = 0
+				}
 			}
 
 			this.playerPassed = true
--- public/js/sprites/player.ts
+++ public/js/sprites/player.ts
@@ -6,16 +6,18 @@ class Player extends Sprite {
 	acc: number
 
 	gravityMultiplier = 1
+	jumpCooldown = 0
 
-	static radius = 20
-	static gravity = 1
-	static leftOffset = 100
-	static jumpVel = 10
+	static radius = 0.02
+	static gravity = 0.001
+	static leftOffset = 0.1
+	static leftDelay = 0.5
+	static jumpAcc = 0.015
 
 	constructor(game: Game) {
 		super()
 		this.game = game
-		this.pos = new Vector([ Player.leftOffset, 100 ])
+		this.pos = new Vector([ 0, Floor.y ])
 		this.vel = 0
 		this.acc = 0
 
@@ -40,36 +42,37 @@ class Player extends Sprite {
 		return this.pos.y + Player.radius
 	}
 
-	move(newAcc: number) {
-		console.log(newAcc)
+	move(newAcc: number, dt: number) {
 		this.acc += Player.gravity * this.gravityMultiplier
 		this.vel += this.acc
-		this.pos.y += this.vel
-		this.pos.x = game.scroll + Player.leftOffset
+		this.pos.y += this.vel * dt * 100
+		this.pos.x = this.game.scroll + Player.leftOffset
 		this.acc = newAcc
 	}
 
 	draw(game: Game) {
 		const topLeft = new Vector([ this.left(), this.top() ])
 		const bottomRight = new Vector([ this.right(), this.bottom() ])
-		game.fillRect(topLeft, bottomRight, '#ff7777')
+		game.fillRect(topLeft, bottomRight, '#ff006e')
 	}
 
-	render(game: Game) {
+	render(game: Game, dt: number) {
 		let newAcc = 0
 
-		if (game.keyboard.isPressed('Space') && this.isOnFloor()) {
-			console.log('space')
-			newAcc -= Player.jumpVel * this.gravityMultiplier
+		if (this.jumpCooldown > 0) this.jumpCooldown--
+
+		if (game.keyboard.isPressed('Space') && this.isOnFloor() && this.jumpCooldown == 0) {
+			newAcc -= Player.jumpAcc * this.gravityMultiplier
+			this.jumpCooldown = 15
 		}
 
-		this.move(newAcc)
+		this.move(newAcc, dt)
 		this.collision()
 		this.draw(game)
 	}
 
 	isOnFloor() {
-		for (let sprite of game.sprites) {
+		for (let sprite of this.game.sprites) {
 			// Todo: use this.top() when inverted gravity
 
 			if (sprite instanceof Floor && this.gravityMultiplier >= 0) {
@@ -78,8 +81,11 @@ class Player extends Sprite {
 			} else if (sprite instanceof Ceiling && this.gravityMultiplier < 0) {
 				if (Math.abs(this.top() - Ceiling.y) < Number.EPSILON)
 					return true
-			} else if (sprite instanceof Platform) {
+			} else if (sprite instanceof Platform && this.gravityMultiplier >= 0) {
 				if (Math.abs(this.bottom() - sprite.topLeft.y) < Number.EPSILON)
+					return true
+			} else if (sprite instanceof Platform && this.gravityMultiplier < 0) {
+				if (Math.abs(this.top() - sprite.bottomRight.y) < Number.EPSILON)
 					return true
 			}
 		}
@@ -88,10 +94,14 @@ class Player extends Sprite {
 	}
 
 	collision() {
-		for (let sprite of game.sprites) {
+		for (let sprite of this.game.sprites) {
 			if (sprite != this) {
 				sprite.collision(this)
 			}
 		}
+	}
+
+	kill() {
+		console.log('you died')
 	}
 }
