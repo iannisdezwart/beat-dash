024987e17f44e9cd3798e66a66e0edcb382d6446
iannisdezwart <iannis__@hotmail.com>
2021-02-15 20:52:13 +0100

    🛠 Updated Loading File

--- public/js/index.ts
+++ public/js/index.ts
@@ -1,8 +1,38 @@
-const game = new Game('#game')
-game.sprites.push(new Floor())
-game.sprites.push(new Ceiling())
-game.sprites.push(new Platform(1000, 500, 80))
-game.sprites.push(new Platform(1500, 1000, 160))
-game.sprites.push(new Platform(2000, 1500, 240))
-game.addPlayer(new Player(game))
-game.start()
+const level = new LevelNostalgia()
+const keyboard = level.game.keyboard
+
+const spaceListenerID = keyboard.onPress('Space', () => {
+	startGame()
+})
+
+const startGame = () => {
+	document.querySelector<HTMLButtonElement>('#play-button').blur()
+	keyboard.deleteOnPress(spaceListenerID)
+	level.game.start()
+}
+
+const getStoredVolume = () => {
+	return parseFloat(localStorage.getItem('volume'))
+}
+
+const setStoredVolume = (newVolume: number) => {
+	localStorage.setItem('volume', newVolume.toString())
+	level.song.setVolume(newVolume)
+	volumeInputLabel.innerText = volumeInput.value + '%'
+}
+
+const volumeInput = document.querySelector<HTMLInputElement>('#volume-slider')
+const volumeInputLabel = document.querySelector<HTMLDivElement>('#volume-slider-value')
+
+// Set volume to stored volume
+
+if (getStoredVolume() == null) {
+	setStoredVolume(1)
+}
+
+setStoredVolume(getStoredVolume())
+
+volumeInput.oninput = () => {
+	const volume = parseInt(volumeInput.value, 10) / 100
+	setStoredVolume(volume)
+}
